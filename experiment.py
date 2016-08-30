086776cf79d1fed350a842137ca721af721012a5
liuxianggen <liuxg16@mails.tsinghua.edu.cn>
2016-08-30 21:37:04 +0800

    ghh

--- experiment.py
+++ experiment.py
@@ -10,6 +10,55 @@ from utils import *
 from model import Memory
 from model import Question
 from model import LocationNet_lxg
+from model import Reasoner_lxg
+
+
+def reasoning_test():
+
+    configs = getattr(config, 'get_config')()
+    train_files = configs['train_file']
+    test_files = configs['test_file']
+
+    x = T.lmatrix()
+    x_mask = T.matrix()
+    y = T.lmatrix()
+    y_mask = T.matrix()
+    l = T.lvector()
+
+    reason = Reasoner_lxg(**config)
+    rate = 0.4
+    cost, decoder_cost = reason.apply(x, x_mask, y, y_mask, l)
+    gcost = cost * (1 - rate) + decoder_cost * rate
+    errors = reason.errors
+
+    params = reason.params
+    for param in params:
+        gcost += T.sum(param ** 2) * 1e-6
+        gcost += T.sum(abs(param)) * 1e-6
+    grads = T.grad(gcost, params)
+
+    updates = adadelta(params, grads)
+
+    fn = theano.function([x, x_mask, y, y_mask, l], [errors], updates=updates)
+    # fn = theano.function([x, x_mask, y, y_mask, l], shapes, updates=updates)
+    test_fn = theano.function([x, x_mask, y, y_mask, l], [errors])
+
+    for _ in range(300):
+        data_class = preprocess(*train_files)
+        test_class = preprocess(*test_files)
+        i = 0
+        sums = 0
+        for facts, question, label in data_class.data_stream():
+            sums += fn(facts[0].T, facts[1].T, question[0].T, question[1].T, label)[0]
+            i += label.shape[0]
+        logger.info("train err {}".format(float(sums) / float(i)))
+        i = 0
+        sums = 0
+        for facts, question, label in test_class.data_stream():
+            sums += test_fn(facts[0].T, facts[1].T, question[0].T, question[1].T, label)[0]
+            i += label.shape[0]
+        logger.info("test err {}".format(float(sums) / float(i)))
+
 def lxg_test():
 
     configs = getattr(config, 'get_config')()
@@ -29,38 +78,36 @@ def lxg_test():
     y_mask = T.matrix()
     l = T.lvector()
 
-    gcost = 0
     memory = Memory(x, x_mask, vocab_size,n_in, n_hids)
     quest = Question(y, y_mask, vocab_size,n_in, n_hids)
     location = LocationNet_lxg(memory, quest,n_hids)
-
+    lt = location.apply()
     gcost = memory.cost
+    gcost += quest.cost
 
     # add regularization
     params = memory.params
-
-
-
+    params += quest.params
 
     for param in params:
         gcost += T.sum(param ** 2) * 1e-6  # l2
         gcost += T.sum(abs(param)) * 1e-6  # l1
     grads = T.grad(gcost, params)
     updates = adadelta(params, grads)
-    fn = theano.function([x, x_mask, y, y_mask, l], [quest.output, memory.length,location.softmax], updates=updates, on_unused_input='ignore')
+    fn = theano.function([x, x_mask, y, y_mask, l], [quest.output, gcost,location.softmax,lt], updates=updates, on_unused_input='ignore')
 
     # test_class = preprocess(*test_files)
     for _ in range(100):
         data_class = preprocess(*train_files)
-        # sums = 0.0
+        sums = 0.0
         i = 0
         for facts, question, label in data_class.data_stream():
-            sums = fn(facts[0].T, facts[1].T, question[0].T, question[1].T, label)[0]
-            print sums.shape
-            break
-        #     i += label.shape[0]
-        # print "train error: {}".format(float(sums / float(i)))
+            sums += fn(facts[0].T, facts[1].T, question[0].T, question[1].T, label)[1]
+
+            i += label.shape[0]
+        print "train error: {}".format(float(sums / float(i)))
 
 
 if __name__=='__main__':
-    lxg_test()
+    # lxg_test()
+    reasoning_test
--- model.py
+++ model.py
@@ -62,84 +62,82 @@ class LocationNet_lxg(object):
 
     def __init__(self, memory, question,n_hids,**kwargs): #(5,39),(39)
         self.W_f = param_init().orth((n_hids, n_hids))
-        self.W_classify = param_init().orth((1, n_hids))  # (5, 39)
+        self.W_classify = param_init().orth((n_hids, 1))  # (39,1)
         self.b_f = param_init().constant((n_hids,))
         self.b_classify = param_init().constant((1,))
-        quest_rep = question.output
+        quest_rep = question.output #(1*39)
+        quest_rep = T.addbroadcast(quest_rep,0)
         mom_rep = T.tanh(theano.dot(memory.output, self.W_f) + self.b_f)#(5*39)
 
         # repeat_x(quest_rep,5)
-        quest_mom = question.output * mom_rep #(5*39)
-
-        mom_rep = T.tanh(theano.dot(quest_mom, self.W_classify) + self.b_classify)  #(5)
+        quest_mom = quest_rep * mom_rep #(5*39)
+        mom_rep = T.tanh(theano.dot(quest_mom, self.W_classify) + self.b_classify)  #(5,1)
 
         self.softmax = T.nnet.softmax(mom_rep) #(5)
-
     # def prepare_inputs():
 
     def apply(self):
-
-        return T.argmax(self.softmax, axis=-1) # (1,)
+        return T.argmax(self.softmax, axis=0) # (1,)
 
 
 # take neural reasoner for example
-# class Reasoner_lxg(object):
-#
-#     def __init__(self, **kwargs):
-#         self.vocab_size = kwargs.pop('vocab_size')
-#         self.n_in = kwargs.pop('nemb')
-#         self.n_hids = kwargs.pop('nhids')
-#         self.n_layer = kwargs.pop('n_layer')
-#         self.n_label = kwargs.pop('label_size')
-#         self.params=[]
-#
-#     def apply(self, facts, facts_mask, question, question_mask, y):
-#
-#         table = lookup_table(self.n_in, self.vocab_size)
-#         self.params += table.params
-#
-#         memory = Memory(facts, facts_mask, self.vocab_size,
-#                                      self.n_in, self.n_hids, table=table)
-#         quest = Question(question, question_mask, self.vocab_size,
-#                                          self.n_in, self.n_hids, table=table)
-#
-#         self.params += memory.params
-#         self.params += quest.params
-#
-#         mem = memory.output #(10,5,39)
-#         que = quest.output #(13,39)
-#
-#         for _ in range(self.n_layer):
-#             que = self.interact(mem, que)
-#
-#         logistic_layer = LogisticRegression(questions_rep,
-#                                             self.n_hids, self.n_label)
-#         self.params += logistic_layer.params
-#         self.cost = logistic_layer.cost(y)/y.shape[0]
-#         self.decoder_cost = facts_encoder.cost + questions_encoder.cost
-#
-#         self.errors = logistic_layer.errors(y)
-#         return self.cost, self.decoder_cost
-#
-#     def interact(self, facts_rep, questions_rep):
-#         self.W_f = param_init().orth((self.n_hids, self.n_hids))
-#         self.W_q = param_init().orth((self.n_hids, self.n_hids)) #(39, 39)
-#         self.b_f = param_init().constant((self.n_hids,))
-#         self.b_q = param_init().constant((self.n_hids,))
-#         self.params += [self.W_f, self.W_q, self.b_f, self.b_q]
-#
-#         questions_rep = T.tanh(theano.dot(questions_rep, self.W_q) + self.b_q)
-#         facts_rep = T.tanh(theano.dot(facts_rep, self.W_f) + self.b_f)
-#
-#         def _one_step(question_rep, facts_rep):
-#             if question_rep.ndim == 1:
-#                 question_rep = T.shape_padleft(question_rep, n_ones=1)
-#             inter_rep = (question_rep + facts_rep).max(axis=0)
-#             return inter_rep
-#
-#         inter_reps, updates = theano.scan(_one_step,
-#                                           sequences=questions_rep,
-#                                           outputs_info=None,
-#                                           non_sequences=facts_rep
-#                                           )
-#         return inter_reps
+class Reasoner_lxg(object):
+
+    def __init__(self, **kwargs):
+        self.vocab_size = kwargs.pop('vocab_size')
+        self.n_in = kwargs.pop('nemb')
+        self.n_hids = kwargs.pop('nhids')
+        self.n_layer = kwargs.pop('n_layer')
+        self.n_label = kwargs.pop('label_size')
+        self.params=[]
+
+    def apply(self, facts, facts_mask, question, question_mask, y):
+
+        table = lookup_table(self.n_in, self.vocab_size)
+        self.params += table.params
+
+        memory = Memory(facts, facts_mask, self.vocab_size,
+                                     self.n_in, self.n_hids, table=table)
+        quest = Question(question, question_mask, self.vocab_size,
+                                         self.n_in, self.n_hids, table=table)
+
+        self.params += memory.params
+        self.params += quest.params
+
+        mem = memory.output #(5,39)
+        que = quest.output #(39)
+
+        for _ in range(self.n_layer):
+            que = self.interact(mem, que)
+
+        logistic_layer = LogisticRegression(que,
+                                            self.n_hids, self.n_label)
+        self.params += logistic_layer.params
+        self.cost = logistic_layer.cost(y)/y.shape[0]
+        self.decoder_cost = mem.cost + que.cost
+
+        self.errors = logistic_layer.errors(y)
+        return self.cost, self.decoder_cost
+
+    def interact(self, facts_rep, questions_rep):
+        self.W_f = param_init().orth((self.n_hids, self.n_hids))
+        self.W_q = param_init().orth((self.n_hids, self.n_hids)) #(39, 39)
+        self.b_f = param_init().constant((self.n_hids,))
+        self.b_q = param_init().constant((self.n_hids,))
+        self.params += [self.W_f, self.W_q, self.b_f, self.b_q]
+
+        questions_rep = T.tanh(theano.dot(questions_rep, self.W_q) + self.b_q)
+        facts_rep = T.tanh(theano.dot(facts_rep, self.W_f) + self.b_f)
+
+        def _one_step(question_rep, facts_rep):
+            if question_rep.ndim == 1:
+                question_rep = T.shape_padleft(question_rep, n_ones=1)
+            inter_rep = (question_rep + facts_rep).max(axis=0)
+            return inter_rep
+
+        inter_reps, updates = theano.scan(_one_step,
+                                          sequences=questions_rep,
+                                          outputs_info=None,
+                                          non_sequences=facts_rep
+                                          )
+        return inter_reps
