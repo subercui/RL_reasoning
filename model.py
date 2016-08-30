ea7a67b7fb7adfd278e0861fdfc95695a74a8e6c
Yukun <yykthu@gmail.com>
2016-08-30 21:30:00 +0800

    add Reasoner RNN in model

--- model.py
+++ model.py
@@ -41,7 +41,26 @@ class LocationNet(object):
 		return self.lt
 
 
-class Reasoner(object):
-	"""the whole model"""
+class Reasoner_RNN(object):
+    def __init__(self, n_qfvector, n_state, n_answer_class):
+        self.n_qfvector = n_qfvector
+        self.n_state = n_state
+        self.n_answer_class = n_answer_class
+        self.dense = Dense(self.n_qfvector, self.n_state)
+        self.stop_sig = Sigmoid(self.n_state)
+        self.answer_sm = Softmax(self.n_state, self.n_answer_class)
+        self.dense._init_params()
+        self.stop_sig._init_params()
+        self.answer_sm._init_params()
 
-	def __init__():
+    def _step_forward(self, qfvector, state_tm1=None, init_flag=False):
+        init_state = T.alloc(numpy.float32(0.), self.n_state)
+        if init_flag:
+            state_tm1 = init_state
+        state = self.dense.apply(qfvector, state_tm1)
+        self.state = state
+        stop = self.stop_sig.apply(self.state)
+        self.stop = stop
+        answer = self.answer_sm.apply(self.state)
+        self.answer = answer
+        return self.state, self.stop, self.answer
